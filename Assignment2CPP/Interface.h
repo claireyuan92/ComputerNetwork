213b3d8aa530fe8bad6663e9687a9c74220f411d
claireyuan92 <meng.yuan@duke.edu>
2015-02-27 20:47:17 -0500

    Modified Assignment2CPP

---
+++ Assignment2CPP/Interface.h
@@ -0,0 +1,64 @@
+//
+//  Interface.h
+//  Routing
+//
+//  Created by Claire on 2/27/15.
+//  Copyright (c) 2015 Claire. All rights reserved.
+//
+
+#ifndef __Routing__Interface__
+#define __Routing__Interface__
+
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <vector>
+#include <stdbool.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <iostream>
+
+#define MTU (1400*8)
+#define BUFLEN (64*1024*8)
+
+class Interface{
+    
+private:
+    //UDP socket
+    sockaddr_in sin;
+    //IP addresses/ports
+    int remote_port;
+    char * my_VIP;
+    char * remote_VIP;
+    char * remote_IP;
+    
+public:
+    
+    Interface(char *line){
+        char *pch;
+        
+        struct hostent *hp;
+        hp=gethostbyname(strtok(line, ":"));
+        remote_IP=strdup(hp->h_addr);
+        
+        pch = strtok (NULL, " ");
+        
+        remote_port=atoi(pch);
+        
+        pch= strtok (NULL, " ");
+        my_VIP = pch;
+        
+        pch= strtok (NULL, " ");
+        remote_VIP = pch;
+    }
+    
+    bool upInterface();
+};
+
+
+#endif /* defined(__Routing__Interface__) */
