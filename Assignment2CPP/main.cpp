f78ae1b97f1c1b531f17e80d02871ada72f9aef0
YuzhouSun <yuzhousun001@gmail.com>
2015-02-28 21:48:29 -0500

    cmd thread

--- Assignment2CPP/main.cpp
+++ Assignment2CPP/main.cpp
@@ -15,13 +15,22 @@
 #include <stdbool.h>
 #include "Node.h"
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+#include<pthread.h> //for threading , link with lpthread
+
+void *cmdReader(void *);
+
+
 #define MAX_ROUTES 200
 #define MAX_TTL 120
 
 
 
 int main(int argc, const char * argv[]) {
-    
+      
     if(argc!=2){
         printf("Usage: Take one file as commandline input\n");
         return 1;
@@ -34,6 +43,25 @@ int main(int argc, const char * argv[]) {
     }
     // Create NODE
     Node mynode(f);
-    mynode.parseCmd("send 10.2.3.4 how are you");
+  cin.clear();
 
+    pthread_t cmdTh ;
+        if( pthread_create( &cmdTh , NULL ,  cmdReader , (void*) &mynode) < 0)
+        {
+            perror("error: create thread");
+            return 1;
+        }
+      
+	pthread_detach( cmdTh);//!!!!!!!!!!!!!!!!!!!!!no possible memmery leak
+	
+}
+
+void * cmdReader(void * mynode){
+  string cmd;
+  Node * node = (Node *) mynode;
+  while(getline(cin,cmd)){
+    //cout<<cmd<<endl;
+    (* node).parseCmd(cmd);
+    cin.clear();
+  }
 }
